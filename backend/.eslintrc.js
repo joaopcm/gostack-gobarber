f28d50a14a80dc6cf817a5bd57e5d89a6f8b4931
João Pedro da Cruz Melo <jopcmelo@gmail.com>
2019-10-14 21:22:53 -0300

    fix: ESLint fix

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -17,10 +17,10 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    'camelcase': 'off',
+    'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }]
   },
 };
