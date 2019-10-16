3faf6a99aab979f0b408b8ebba92bce493038ef9
João Pedro da Cruz Melo <jopcmelo@gmail.com>
2019-10-15 23:26:28 -0300

    feat: Add a failure handler to Redis processes

--- backend/src/app/jobs/CancellationMail.js
+++ backend/src/app/jobs/CancellationMail.js
@@ -10,8 +10,6 @@ class CancellationMail {
   async handle({ data }) {
     const { appointment } = data;
 
-    console.log('A fila executou!');
-
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
