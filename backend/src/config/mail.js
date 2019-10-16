524fceca61ef2fa8d36f1fb675e06c4cba2a86f4
João Pedro da Cruz Melo <jopcmelo@gmail.com>
2019-10-15 22:01:53 -0300

    feat: Add a mailer service

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '8ebf69458c0ff2',
+    pass: '034ddc5aa19e82',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
