fa060556b3514fdf684ccf90d72963468827d923
Joao Melo <jopcmelo@gmail.com>
2019-11-09 19:51:41 -0300

    feat: Add update profile feature

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
   function handleSubmit(data) {
-    // ...
+    dispatch(updateProfileRequest(data));
   }
 
   return (
--- frontend/src/store/modules/user/sagas.js
+++ frontend/src/store/modules/user/sagas.js
@@ -1,3 +1,29 @@
-import { all } from 'redux-saga/effects';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Profile updated');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (error) {
+    toast.error(error.response.data.error);
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
