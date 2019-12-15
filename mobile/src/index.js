082d5e725ba344c76e8d6d677190e510812f9564
João Melo <jopcmelo@gmail.com>
2019-12-15 15:44:13 -0300

    feat: Add Redux

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
 import './config/reactotronConfig';
 
+import { store, persistor } from './store';
 import Routes from './routes';
 
 import colors from '~/styles/colors';
 
 export default function src() {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor={colors.dark} />
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor={colors.dark} />
+        <Routes />
+      </PersistGate>
+    </Provider>
   );
 }
---
+++ mobile/src/store/modules/auth/sagas.js
@@ -0,0 +1,85 @@
+import { Alert } from 'react-native';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
+import api from '~/services/api';
+import { signFailure, signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    if (user.provider) {
+      Alert.alert(
+        'Erro no login',
+        'O usuário não pode ser prestador de serviços'
+      );
+      return;
+    }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield put(signInSuccess(token, user));
+
+    // history.push('/dashboard');
+    return;
+  } catch (error) {
+    Alert.alert(
+      'Falha na autenticação',
+      'Houve um erro no login, verifique seus dados'
+    );
+    yield put(signFailure());
+  }
+}
+
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    Alert.alert(
+      'Conta criada com sucesso',
+      'Sua nova conta foi criada com sucesso'
+    );
+
+    // history.push('/');
+  } catch (error) {
+    Alert.alert(
+      'Falha no cadastro',
+      'Houve um erro no cadastro, verifique seus dados'
+    );
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  // history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
